d2382ae648e721af743e67c15085145dbc2b9112
INNOVATION LAB-09 <INNOVATION LAB-09>
2017-11-02 12:56:27 +0530

    Updated contracts as per scope of v2.0

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,25 @@
+var assetRegister = artifacts.require("./AssetRegister.sol");
+var assetRequest = artifacts.require("./AssetRequest.sol");
+var assetToken = artifacts.require("./AssetToken.sol");
+var BidRequests = artifacts.require("./BidRequests.sol");
+var CToken = artifacts.require("./CToken.sol");
+var MPAssetRequests = artifacts.require("./MPAssetRequests.sol");
+var REAssets = artifacts.require("./REAssets.sol");
+var RegReceipts = artifacts.require("./RegReceipts.sol");
+var TxRegister = artifacts.require("./TxRegister.sol");
+var Users = artifacts.require("./Users.sol");
+
+
+module.exports = function(deployer) {
+  deployer.deploy(assetRegister);
+  deployer.deploy(assetRequest);
+  deployer.link(assetRegister,assetToken);
+  deployer.deploy(assetToken);
+  deployer.deploy(BidRequests);
+  deployer.deploy(CToken);
+  deployer.deploy(MPAssetRequests);
+  deployer.deploy(REAssets);
+  deployer.deploy(RegReceipts);
+  deployer.deploy(TxRegister);
+  deployer.deploy(Users);
+};
